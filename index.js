a6563d8cd2f995c73aabcb6efc7b82b9f55598d0
kelly_maass <kmaass77627@gmail.com>
2020-05-12 10:50:05 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-// Code your solutions in this file
+
+  function writeCards(names, event) {
+    for (let count = 0; count < names.length; count++) {
+      console.log(`Thank you, ${name}, for the wonderful birthday gift!`)
+    }
+    return names
+  }
+
+ 
+  let countdown = 0;
+while (countdown > 0) {
+  console.log(countdown--);
+}
